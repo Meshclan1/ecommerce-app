dd422797ca477d44b2e0999db35fd2182c58a542
Ayo <Ayo.meshe@gmail.com>
2024-03-28 18:33:53 +0000

    nav page component setup

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Nav></Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+}
